ad6127e5c7307ee930d260e8ba51f06eb9c809ab
Dmitry Khasanov <pik4ez@gmail.com>
2016-01-04 05:02:46 +1100

    Config added.

--- main.go
+++ main.go
@@ -4,11 +4,40 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/BurntSushi/toml"
 	"github.com/pik4ez/drysh/plugin"
 	_ "github.com/pik4ez/drysh/pluginimport"
 )
 
+type out struct {
+	Pattern string
+	Plugin  string
+	Config  toml.Primitive
+}
+
+type config struct {
+	Outs []out `toml:"out"`
+}
+
+var configFile = "/home/vagrant/tmp/drysh_build/config.toml"
+
 func main() {
+	var cfg config
+	var md toml.MetaData
+	md, err := toml.DecodeFile(configFile, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, o := range cfg.Outs {
+		out_plugin := plugin.GetOut(o.Plugin)
+		out_cfg_prim := o.Config
+		var plugin_conf = out_plugin.GetConfig()
+		md.PrimitiveDecode(out_cfg_prim, plugin_conf)
+	}
+
 	out := plugin.GetOut("outfile")
 	result := out.Write("somestring")
 	fmt.Println(result)
--- out/out.go
+++ out/out.go
@@ -2,4 +2,5 @@ package out
 
 type Out interface {
 	Write(data string) string
+	GetConfig() interface{}
 }
